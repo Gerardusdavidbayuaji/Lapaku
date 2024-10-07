3d1d179f398a2dfffc573881ccbed1ca99fcc0b0
Gerardusdavid <142146086+Gerardusdavidbayuaji@users.noreply.github.com>
2024-10-07 17:42:01 +0700

    feat: update components

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import LinksDropDown from "./LinksDropDown";
+import Container from "../global/Container";
+import CartButton from "./CartButton";
+import NavSearch from "./NavSearch";
+import DarkMode from "./DarkMode";
+import Logo from "./Logo";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <nav className="border-b">
+      <Container className="flex flex-col sm:flex-row sm: justify-between sm:items-center flex-wrap gap-4 py-8">
+        <Logo />
+        <NavSearch />
+        <div className="flex gap-4 items-center">
+          <CartButton />
+          <DarkMode />
+          <LinksDropDown />
+        </div>
+      </Container>
+    </nav>
+  );
 }
 
 export default Navbar;
