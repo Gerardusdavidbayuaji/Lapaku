1d7076686386866e3d037e93e6e4616377a46b34
Gerardusdavid <142146086+Gerardusdavidbayuaji@users.noreply.github.com>
2024-10-09 22:03:22 +0700

    feat: update components

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
+import LoadingContainer from "@/components/global/LoadingContainer";
 import FeaturedProducts from "@/components/home/FeaturedProducts";
 import Hero from "@/components/home/Hero";
-import React from "react";
+import { Suspense } from "react";
 
-function HomePage() {
+function HomPage() {
   return (
     <>
       <Hero />
-      <FeaturedProducts />
+      <Suspense fallback={<LoadingContainer />}>
+        <FeaturedProducts />
+      </Suspense>
     </>
   );
 }
-
-export default HomePage;
+export default HomPage;
