2a90685bb1ce73d53b6ecd0e9380eb2de947dd67
Gerardusdavid <142146086+Gerardusdavidbayuaji@users.noreply.github.com>
2024-10-14 18:15:13 +0700

    feat: create product

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,7 +1,10 @@
 "use server";
 
 import db from "@/utils/db";
+import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
+import { imageSchema, productSchema, validateWithZodSchema } from "./schemas";
+import { uploadImage } from "./supabase";
 
 export const fetchFeaturedProducts = async () => {
   const products = await db.product.findMany({
@@ -39,9 +42,57 @@ export const fetchSingleProduct = async (productId: string) => {
   return product;
 };
 
+const renderError = (error: unknown): { message: string } => {
+  console.log(error);
+  return {
+    message: error instanceof Error ? error.message : "An error occurred",
+  };
+};
+
+const getAuthUser = async () => {
+  const user = await currentUser();
+  if (!user) redirect("/");
+  return user;
+};
+
+const getAdminUser = async () => {
+  const user = await getAuthUser();
+  if (user.id === process.env.ADMIN_USER_ID) redirect("/");
+  return user;
+};
+
 export const createProductAction = async (
   prevState: any,
   formData: FormData
 ): Promise<{ message: string }> => {
-  return { message: "product created" };
+  const user = await getAuthUser();
+
+  try {
+    const rawData = Object.fromEntries(formData);
+    const file = formData.get("image") as File;
+    const validatedFields = validateWithZodSchema(productSchema, rawData);
+    const validatedFile = validateWithZodSchema(imageSchema, { image: file });
+    const fullpath = await uploadImage(validatedFile.image);
+
+    await db.product.create({
+      data: {
+        ...validatedFields,
+        image: fullpath,
+        clerkId: user.id,
+      },
+    });
+  } catch (error) {
+    return renderError(error);
+  }
+  redirect("/admin/products");
+};
+
+export const fetchAdminProducts = async () => {
+  await getAdminUser();
+  const products = await db.product.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+  return products;
 };
