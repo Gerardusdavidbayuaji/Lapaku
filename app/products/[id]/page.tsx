66cd9f39b01229c7201079d3ef3f776cd9dd8f5e
Gerardusdavid <142146086+Gerardusdavidbayuaji@users.noreply.github.com>
2024-10-15 16:50:33 +0700

    feat: add share product

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,10 +1,12 @@
-import BreadCrumbs from "@/components/single-product/BreadCrumbs";
-import { fetchSingleProduct } from "@/utils/actions";
-import Image from "next/image";
-import { formatCurrency } from "@/utils/format";
 import FavoriteToggleButton from "@/components/products/FavoriteToggleButton";
-import AddToCart from "@/components/single-product/AddToCart";
 import ProductRating from "@/components/single-product/ProductRating";
+import ShareButton from "@/components/single-product/ShareButton";
+import BreadCrumbs from "@/components/single-product/BreadCrumbs";
+import AddToCart from "@/components/single-product/AddToCart";
+import { fetchSingleProduct } from "@/utils/actions";
+import { formatCurrency } from "@/utils/format";
+import Image from "next/image";
+
 async function SingleProductPage({ params }: { params: { id: string } }) {
   const product = await fetchSingleProduct(params.id);
   const { name, image, company, description, price } = product;
@@ -28,7 +30,10 @@ async function SingleProductPage({ params }: { params: { id: string } }) {
         <div>
           <div className="flex gap-x-8 items-center">
             <h1 className="capitalize text-3xl font-bold">{name}</h1>
-            <FavoriteToggleButton productId={params.id} />
+            <div className="flex items-center gap-x-2">
+              <FavoriteToggleButton productId={params.id} />
+              <ShareButton name={product.name} productId={params.id} />
+            </div>
           </div>
           <ProductRating productId={params.id} />
           <h4 className="text-xl mt-2">{company}</h4>
